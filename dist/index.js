67f383177505c463a2fef5dee7da79358844f77d
misawa-shota <kaiyou0120@gmail.com>
2025-11-21 23:34:15 +0900

    演算子の学習

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,13 @@
-const message = `Hello
-world!`;
-console.log(message);
-export {};
+import readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const rl = readline.createInterface({
+    input,
+    output
+});
+(async () => {
+    const name = await rl.question('名前を入力してください:');
+    const displayName = name ?? "名無し";
+    console.log(`こんにちは、${displayName}さん！`);
+    rl.close();
+})();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const message: string = `Hello
-world!`;
-console.log(message);
+import readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({
+  input,
+  output
+});
+
+(async () => {
+  const name = await rl.question('名前を入力してください:');
+  const displayName = name ?? "名無し";
+  console.log(`こんにちは、${displayName}さん！`);
+  rl.close();
+})();
