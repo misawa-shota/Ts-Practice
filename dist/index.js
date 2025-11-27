723b851766e5a08cebeab32d0d5eac4ae07ca2b5
misawa-shota <kaiyou0120@gmail.com>
2025-11-27 18:10:51 +0900

    オプショナルなプロパティの宣言

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,15 @@
-import readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-const rl = readline.createInterface({
-    input,
-    output
-});
-(async () => {
-    const input = await rl.question('名前を入力してください:');
-    const name = input ? input : "名無し";
-    const user = {
-        name,
-        age: 20,
-    };
-    console.log(user);
-    rl.close();
-})();
+// import readline from 'node:readline/promises';
+// import { stdin as input, stdout as output } from 'node:process';
+const obj1 = {
+    foo: true,
+    bar: false,
+};
+const obj2 = {
+    foo: false,
+    bar: true,
+    baz: 42,
+};
+console.log(obj1.baz);
+console.log(obj2.baz);
+export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
-import readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+// import readline from 'node:readline/promises';
+// import { stdin as input, stdout as output } from 'node:process';
 
-const rl = readline.createInterface({
-  input,
-  output
-});
+// const rl = readline.createInterface({
+//   input,
+//   output
+// });
 
-(async () => {
-  const input = await rl.question('名前を入力してください:');
-  const name = input ? input : "名無し";
-  const user = {
-    name,
-    age: 20,
-  };
-  console.log(user);
-  rl.close();
-})();
+// (async () => {
+//   const input = await rl.question('名前を入力してください:');
+//   const name = input ? input : "名無し";
+//   const user = {
+//     name,
+//     age: 20,
+//   };
+//   console.log(user);
+//   rl.close();
+// })();
+
+type MyObj = {
+  foo: boolean,
+  bar: boolean,
+  baz?:number,  
+};
+
+const obj1: MyObj = {
+  foo: true,
+  bar: false,
+};
+
+const obj2: MyObj = {
+  foo: false,
+  bar: true,
+  baz: 42,
+};
+
+console.log(obj1.baz);
+console.log(obj2.baz);
