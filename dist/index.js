345f451f96a916a51d17568a115778453323f276
misawa-shota <kaiyou0120@gmail.com>
2025-12-05 17:21:47 +0900

    分割代入を使った関数式

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,17 @@
 // import readline from 'node:readline/promises';
 // import { stdin as input, stdout as output } from 'node:process';
 // function関数式
-function calcBmi(human) {
-    return human.weight / human.height ** 2;
+// function calcBmi(human: Human): number {
+//   return human.weight / human.height ** 2;
+// }
+// const uhyo: Human = {
+//   height: 1.75,
+//   weight: 60,
+// };
+// console.log(calcBmi(uhyo));
+//　分割代入
+function calcBmi({ height, weight }) {
+    return weight / height ** 2;
 }
 const uhyo = {
     height: 1.75,
--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,20 @@ type Human = {
 };
 
 // function関数式
-function calcBmi(human: Human): number {
-  return human.weight / human.height ** 2;
+// function calcBmi(human: Human): number {
+//   return human.weight / human.height ** 2;
+// }
+
+// const uhyo: Human = {
+//   height: 1.75,
+//   weight: 60,
+// };
+
+// console.log(calcBmi(uhyo));
+
+//　分割代入
+function calcBmi({height, weight}: Human): number {
+  return weight / height ** 2;
 }
 
 const uhyo: Human = {
