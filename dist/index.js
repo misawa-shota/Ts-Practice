7b60d8c8ec54573eac1f57ea38bf9caa0ff44624
misawa-shota <kaiyou0120@gmail.com>
2025-12-11 22:16:53 +0900

    implementsキーワードによるクラスの型チェック

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,13 @@
 class User {
     name;
-    age;
+    #age;
     constructor(name, age) {
         this.name = name;
-        this.age = age;
+        this.#age = age;
     }
     isAdult() {
-        return this.age >= 20;
+        return this.#age >= 20;
     }
 }
-class PremiumUser extends User {
-    rank;
-    constructor(name, age, rank) {
-        super(name, age);
-        this.rank = rank;
-    }
-    isAdult() {
-        return this.age >= 18;
-    }
-}
-const jhon = new User("jhon", 15);
-const uhyo = new PremiumUser("uhyo", 26, 5);
-console.log(uhyo.isAdult());
-console.log(jhon.isAdult());
-console.log(uhyo.age); // Error: 'age' is protected and only accessible within class 'User' and its subclasses.
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,17 @@
-class User {
+type HasName = {
   name: string;
-  protected age: number;
+};
+
+class User implements HasName {
+  name: string;
+  #age: number;
 
   constructor (name: string, age: number) {
     this.name = name;
-    this.age = age;
+    this.#age = age;
   }
 
   public isAdult(): boolean {
-    return this.age >= 20;
+    return this.#age >= 20;
   }
 }
-
-class PremiumUser extends User {
-  rank: number;
-
-  constructor (name: string, age: number, rank: number) {
-    super(name, age);
-    this.rank = rank;
-  }
-
-  public override isAdult(): boolean {
-    return this.age >= 18;
-  }
-}
-
-const jhon = new User("jhon", 15);
-const uhyo = new PremiumUser("uhyo", 26, 5);
-
-console.log(uhyo.isAdult());
-console.log(jhon.isAdult());
-console.log(uhyo.age); // Error: 'age' is protected and only accessible within class 'User' and its subclasses.
