d8d5c68c6d7a1c6a40115f6be0bd21e2e6936412
misawa-shota <kaiyou0120@gmail.com>
2025-12-21 10:45:50 +0900

    型の絞り込み

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,16 @@
-const uhyo1 = "uhyo";
-let uhyo2 = "uhyo";
+function signNumber(type) {
+    return type === "plus" ? 1 : -1;
+}
+function numberWithSign(num, type) {
+    if (type === "none") {
+        return 0;
+    }
+    else {
+        return num * signNumber(type);
+    }
+}
+console.log(numberWithSign(5, "plus"));
+console.log(numberWithSign(5, "minus"));
+console.log(numberWithSign(5, "none"));
 export {};
 //# sourceMappingURL=index.js.map
