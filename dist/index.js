11f0f671c5134fa46c0f8e3e9fb1daef08279915
misawa-shota <kaiyou0120@gmail.com>
2025-12-25 13:36:50 +0900

    ユーザー定義型ガード（asserts記法）niyoru による型の絞り込み

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,23 @@
-function isStringOrNumber(value) {
-    return typeof value === 'string' || typeof value === 'number';
+function assertHuman(value) {
+    if (value == null) {
+        throw new Error("Given value is null or undefined");
+    }
+    if (value.type !== "Human" ||
+        typeof value.name !== "string" ||
+        typeof value.age !== "number") {
+        throw new Error("Given value is not a valid Human object");
+    }
 }
-const something = 123;
-if (isStringOrNumber(something)) {
-    console.log(something.toString());
+function checkAndUserHuman(value) {
+    assertHuman(value);
+    const name = value.name;
+    console.log(`Human's name is ${name}`);
 }
+const uhyo = {
+    type: "Human",
+    name: "uhyo",
+    age: 30,
+};
+checkAndUserHuman(uhyo);
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-function isStringOrNumber(value: unknown): value is string | number {
-  return typeof value === 'string' || typeof value === 'number';
+type Human = {
+  type: "Human";
+  name: string;
+  age: number;
 }
 
-const something: unknown = 123;
+function assertHuman(value: any): asserts value is Human {
+  if (value == null) {
+    throw new Error("Given value is null or undefined");
+  }
 
-if (isStringOrNumber(something)) {
-  console.log(something.toString());
+  if (
+    value.type !== "Human" ||
+    typeof value.name !== "string" ||
+    typeof value.age !== "number"
+  ) {
+    throw new Error("Given value is not a valid Human object");
+  }
 }
+
+function checkAndUserHuman(value: unknown) {
+  assertHuman(value);
+  const name = value.name;
+  console.log(`Human's name is ${name}`);
+}
+
+const uhyo = {
+  type: "Human",
+  name: "uhyo",
+  age: 30,
+};
+
+checkAndUserHuman(uhyo);
