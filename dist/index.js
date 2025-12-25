cd62b80ad3c50715c12578923db118048e13e809
misawa-shota <kaiyou0120@gmail.com>
2025-12-25 14:18:20 +0900

    mapped typesによる型の指定

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
-const arr1 = [42, "hello", "world"];
-const arr2 = [7];
+const numbers = {
+    apple: ["apple", "apple"],
+    orange: ["orange", "orange", "orange"],
+    banana: ["banana"],
+};
+console.log(numbers);
 export {};
-// error examples
-// const arr3: NumberAndStrings = []; // Error: Tuple type '[number, ...string[]]' of length '1' has no element at index '0'.
-// const arr4: NumberAndStrings = ["not a number", "hello"]; // Error: Type 'string' is not assignable to type 'number'.
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-type NumberAndStrings = [number, ...string[]];
+type Fruit = "apple" | "orange" | "banana";
 
-const arr1: NumberAndStrings = [42, "hello", "world"];
-const arr2: NumberAndStrings = [7];
+// type FruitNumbers = {
+//   [P in Fruit]: number;
+// };
 
-// error examples
-// const arr3: NumberAndStrings = []; // Error: Tuple type '[number, ...string[]]' of length '1' has no element at index '0'.
-// const arr4: NumberAndStrings = ["not a number", "hello"]; // Error: Type 'string' is not assignable to type 'number'.
+// const numbers: FruitNumbers = {
+//   apple: 5,
+//   orange: 10,
+//   banana: 15,
+// };
+
+// console.log(numbers);
+
+type FruitArrays = {
+  [P in Fruit]: P[];
+};
+
+const numbers: FruitArrays = {
+  apple: ["apple", "apple"],
+  orange: ["orange", "orange", "orange"],
+  banana: ["banana"],
+};
+
+console.log(numbers);
