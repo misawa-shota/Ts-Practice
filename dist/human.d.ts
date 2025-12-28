02642b3fd648948d088f528e251e0ba3b9df981f
misawa-shota <kaiyou0120@gmail.com>
2025-12-28 18:23:20 +0900

    moduleのimport,export

---
+++ dist/human.d.ts
@@ -0,0 +1,5 @@
+export type Human = {
+    name: string;
+    age: number;
+};
+//# sourceMappingURL=human.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-import * as uhyo from "./uhyo.js";
+import { readFile } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-console.log(uhyo.name);
-console.log(uhyo.age);
+const filePath = fileURLToPath(import.meta.url);
+const dirPath = path.dirname(filePath);
+const targetPath = path.join(dirPath, "../uhyo.txt");
+console.log(filePath);
+console.log(dirPath);
+console.log(targetPath);
+
+readFile(targetPath, "utf-8", (err, data) => {
+  let count = 0;
+  let currentIndex = 0;
+  if(data) {
+    while (true) {
+      const nextIndex = data.indexOf("uhyo", currentIndex);
+      if (nextIndex >= 0) {
+        count++;
+        currentIndex = nextIndex + 1;
+      } else {
+        break;
+      }
+    }
+  } else {
+    console.log(err);
+  }
+  console.log(count);
+});
