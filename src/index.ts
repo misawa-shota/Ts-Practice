9fb621aa7e58e4c2f4c65a8181cb8f0bb898b108
misawa-shota <kaiyou0120@gmail.com>
2025-12-30 19:25:33 +0900

    Promise.all

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { readFile } from "fs/promises";
 
-const p = readFile("./uhyo.txt", "utf-8");
+const p = Promise.all([
+  readFile("./foo.txt", "utf-8"),
+  readFile("./bar.txt", "utf-8"),
+  readFile("./baz.txt", "utf-8"),
+]);
 
-p.then((result) => {
-  console.log("成功", result);
-});
-p.catch((error: unknown) => {
-  console.log("失敗", error);
-});
-p.finally(() => {
-  console.log("処理終了");
+p.then(([foo, bar, baz]) => {
+  console.log("foo.txt", foo);
+  console.log("bar.txt", bar);
+  console.log("baz.txt", baz);
 });
