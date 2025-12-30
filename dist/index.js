b2839f5cd2b5171069a4a3950092835902bf0f7c
misawa-shota <kaiyou0120@gmail.com>
2025-12-30 13:49:33 +0900

    Promise, error, finally

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 import { readFile } from "fs/promises";
 const p = readFile("./uhyo.txt", "utf-8");
-p.then((data) => {
-    console.log(data);
+p.then((result) => {
+    console.log("成功", result);
+});
+p.catch((error) => {
+    console.log("失敗", error);
+});
+p.finally(() => {
+    console.log("処理終了");
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { readFile } from "fs/promises";
 
 const p = readFile("./uhyo.txt", "utf-8");
 
-p.then((data) => {
-  console.log(data);
+p.then((result) => {
+  console.log("成功", result);
+});
+p.catch((error: unknown) => {
+  console.log("失敗", error);
+});
+p.finally(() => {
+  console.log("処理終了");
 });
