0f0c7abda4a9c489265745fc7f64017c8ecc1997
misawa-shota <kaiyou0120@gmail.com>
2025-12-30 12:51:23 +0900

    Promiseを使用した非同期処理

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,6 @@
-import { readFile } from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-const filePath = fileURLToPath(import.meta.url);
-const dirPath = path.dirname(filePath);
-const targetPath = path.join(dirPath, "../uhyo.txt");
-console.log(filePath);
-console.log(dirPath);
-console.log(targetPath);
-readFile(targetPath, "utf-8", (err, data) => {
-    let count = 0;
-    let currentIndex = 0;
-    if (data) {
-        while (true) {
-            const nextIndex = data.indexOf("uhyo", currentIndex);
-            if (nextIndex >= 0) {
-                count++;
-                currentIndex = nextIndex + 1;
-            }
-            else {
-                break;
-            }
-        }
-    }
-    else {
-        console.log(err);
-    }
-    console.log(count);
+import { readFile } from "fs/promises";
+const p = readFile("./uhyo.txt", "utf-8");
+p.then((data) => {
+    console.log(data);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-import { readFile } from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { readFile } from "fs/promises";
 
-const filePath = fileURLToPath(import.meta.url);
-const dirPath = path.dirname(filePath);
-const targetPath = path.join(dirPath, "../uhyo.txt");
-console.log(filePath);
-console.log(dirPath);
-console.log(targetPath);
+const p = readFile("./uhyo.txt", "utf-8");
 
-readFile(targetPath, "utf-8", (err, data) => {
-  let count = 0;
-  let currentIndex = 0;
-  if(data) {
-    while (true) {
-      const nextIndex = data.indexOf("uhyo", currentIndex);
-      if (nextIndex >= 0) {
-        count++;
-        currentIndex = nextIndex + 1;
-      } else {
-        break;
-      }
-    }
-  } else {
-    console.log(err);
-  }
-  console.log(count);
+p.then((data) => {
+  console.log(data);
 });
