9e595f16fd33419a8fa2b6a88a026888d37044e7
misawa-shota <kaiyou0120@gmail.com>
2025-12-31 17:18:59 +0900

    dynamic import構文を使用したモジュールの読み込み

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-import { readFile } from "fs/promises";
-const p = readFile("../foo.txt", "utf-8");
-const p2 = p.then((result) => {
-    console.log("成功", result);
+import("fs/promises")
+    .then(({ readFile }) => readFile("./foo.txt", "utf-8"))
+    .then((data) => console.log(data))
+    .catch((err) => console.error("Error reading foo.txt:", err))
+    .finally(() => {
+    console.log("Finished attempting to read file.");
 });
-const p3 = p2.catch((error) => {
-    console.log("失敗", error);
-});
+export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { readFile } from "fs/promises";
-
-const p = readFile("../foo.txt", "utf-8");
-
-const p2 = p.then((result) => {
-  console.log("成功", result);
-});
-const p3 = p2.catch((error) => {
-  console.log("失敗", error);
-});
+import("fs/promises")
+  .then(({ readFile }) => readFile("./foo.txt", "utf-8"))
+  .then((data) => console.log(data))
+  .catch((err) => console.error("Error reading foo.txt:", err))
+  .finally(() => {
+    console.log("Finished attempting to read file.");
+  });
