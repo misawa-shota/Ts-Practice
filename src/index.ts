39db8f9497b3b9f23d9f7c10fd7badfc64157083
misawa-shota <kaiyou0120@gmail.com>
2025-12-31 18:31:41 +0900

    async関数とawait式を使った非同期処理

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import("fs/promises")
-  .then(({ readFile }) => readFile("./foo.txt", "utf-8"))
-  .then((data) => console.log(data))
-  .catch((err) => console.error("Error reading foo.txt:", err))
-  .finally(() => {
-    console.log("Finished attempting to read file.");
-  });
+async function main() {
+  try {
+    const { readFile, writeFile } = await import("fs/promises");
+    const fooContent = await readFile("./foo.txt", "utf-8");
+    await writeFile("bar.txt", fooContent);
+    console.log("書き込み完了しました。");
+  } catch {
+    console.log("失敗しました");
+  }
+}
+
+main().then((() => {
+  console.log("main()が完了しました。");
+}));
